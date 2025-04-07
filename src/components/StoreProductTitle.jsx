9dcbd000abe73e9c8838e11f0c1c692d814edea1
Dozy <dotuan14747@gmail.com>
2025-04-07 17:33:53 +0300

    Created StoreProductTitle.jsx component to display product title in Store. Added StoreProductTitle to Store.js

---
+++ src/components/StoreProductTitle.jsx
@@ -0,0 +1,24 @@
+import { View, Text } from "react-native";
+import { ThemeColors } from "../utils/ThemeColors";
+
+export const StoreProductTitle = ({ title }) => {
+  return (
+    <View
+      style={{
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+      }}
+    >
+      <Text
+        style={{
+          fontSize: 20,
+          fontWeight: "600",
+          color: ThemeColors.third,
+        }}
+      >
+        {title}
+      </Text>
+    </View>
+  );
+};
--- src/screens/Store.js
+++ src/screens/Store.js
@@ -1,10 +1,20 @@
-import { Text, View } from "react-native";
+import { ScrollView, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { ThemeColors } from "../utils/ThemeColors";
+import { StoreProductTitle } from "../components/StoreProductTitle";
 
 const Store = () => {
   return (
-    <View>
-      <Text>Store</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: ThemeColors.secondary }}>
+      <ScrollView
+        style={{ flex: 1, paddingHorizontal: 20, paddingTop: 10 }}
+        showsVerticalScrollIndicator={false}
+      >
+        <View style={{ gap: 20 }}>
+          <StoreProductTitle title="Fruits" />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
